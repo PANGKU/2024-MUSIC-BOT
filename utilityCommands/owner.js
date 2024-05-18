cbf592f1f3e94c8194d2eaf9c1ec49aeb212bc01
PANGKU <62094874+PANGKU@users.noreply.github.com>
2024-05-18 17:19:08 +0600

    Update owner.js

--- utilityCommands/owner.js
+++ utilityCommands/owner.js
@@ -4,12 +4,11 @@ module.exports = {
     name: 'owner',
     description: 'Bot owner info',
     execute(message, args) {
-        const youtubeLink = 'https://www.youtube.com/@GlaceYT';
-        const InstagramLink = 'https://www.instagram.com/glaceytt';
+        const InstagramLink = 'https://www.instagram.com/mr_pangku/';
         const embed = new EmbedBuilder()
             .setColor('#FFFFFF')
             .setTitle(' 🫅 Owner Info')
-            .setDescription(`__**About me**__:\n 🤖 Myself Shiva aka GlaceYT. I am a discord bot developer and web developer. I love playing games, watching anime and building different webserver applications. You will get faster replies on instagram than other social media. Feel free to contact me!\n ❤️ [GlceYT](${youtubeLink})\n 💙 [GlaceYTT](${InstagramLink})`)
+            .setDescription(`__**About me**__:\n 🤖 Myself Pangku. I am a discord bot developer and web developer. I love playing games, watching anime and building different webserver applications. You will get faster replies on instagram than other social media. Feel free to contact me!\n ❤️ [GlceYT](${youtubeLink})\n 💙 [GlaceYTT](${InstagramLink})`)
             .setTimestamp();
 
 
