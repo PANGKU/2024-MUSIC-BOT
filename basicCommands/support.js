61952ae17e05e26c2cd7558f47d940bab1e01ec5
PANGKU <62094874+PANGKU@users.noreply.github.com>
2024-06-24 02:54:51 +0600

    Update support.js

--- basicCommands/support.js
+++ basicCommands/support.js
@@ -4,18 +4,18 @@ module.exports = {
     name: 'support',
     description: 'support server of this Bot',
     execute(message, args) {
-        const supportServerLink = "https://discord.gg/xQF9f9yUEM";
-      const githubLink = "https://github.com/GlaceYT";
-      const replitLink = "https://replit.com/@GlaceYT";
-      const youtubeLink = "https://www.youtube.com/@GlaceYT";
+        const supportServerLink = "https://discord.gg/AKdjNvfngd";
+      const facebookLink = "https://github.com/GlaceYT";
+      const instagramLink = "https://www.instagram.com/mr_pangku";
+      const twitchLink = "https://www.twitch.tv/ven_pangku";
         const embed = new EmbedBuilder()
             .setColor('#b300ff')
             .setAuthor({
               name: 'Support Server',
               iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230824519220985896/6280-2.gif?ex=6638ae28&is=66375ca8&hm=13e4a1b91a95b2934a39de1876e66c11711c7b30ac1a91c2a158f2f2ed1c2fc6&', 
-              url: 'https://discord.gg/xQF9f9yUEM'
+              url: 'https://discord.gg/AKdjNvfngd'
           })
-            .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- GitHub - ${githubLink}\n- Replit - ${replitLink}\n- YouTube - ${youtubeLink}`)
+            .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- GitHub - ${facebookLink}\n- Replit - ${instagramLink}\n- YouTube - ${twitchLink}`)
             .setImage('https://cdn.discordapp.com/attachments/913407674597642250/1242594273111576698/ezgif-6-3a08fef5ca.gif?ex=66794058&is=6677eed8&hm=2b5678af9ac10181460af9f47f11dd4b2c13b0727cc83f53f417c039db89da5e&')
             .setTimestamp();
 
