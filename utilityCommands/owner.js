483930d75a8a701c68f19ad35cd3877f69f4a748
PANGKU <62094874+PANGKU@users.noreply.github.com>
2024-07-11 04:56:21 +0600

    Update owner.js

--- utilityCommands/owner.js
+++ utilityCommands/owner.js
@@ -4,12 +4,12 @@ module.exports = {
     name: 'owner',
     description: 'Bot owner info',
     execute(message, args) {
-        const facebookLink = 'https://www.facebook.com/pangku69/';
+        const FacebookLink = 'https://www.facebook.com/pangku69';
         const InstagramLink = 'https://www.instagram.com/mr_pangku';
         const embed = new EmbedBuilder()
             .setColor('#FFFFFF')
             .setTitle(' 🫅 Owner Info')
-            .setDescription(`__**About me**__:\n 🤖 Myself Rafi Ahmad aka Pangku. I am a discord bot developer and web developer. I love playing games, watching anime and building different webserver applications. You will get faster replies on instagram than other social media. Feel free to contact me!\n ❤️ [Rafi Ahmaf](${facebookLink})\n 💙 [Pangku](${InstagramLink})`)
+            .setDescription(`__**About me**__:\n 🤖 Myself Rafi Ahmad aka Pangku. I am a discord bot developer and web developer. I love playing games, watching anime and building different webserver applications. You will get faster replies on instagram than other social media. Feel free to contact me!\n ❤️ [Rafi Ahmad](${FacebookLink})\n 💙 [Pangku](${InstagramLink})`)
             .setTimestamp();
 
 
